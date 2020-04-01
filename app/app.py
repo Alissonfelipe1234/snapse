251a307198753847f09a18dbc9ad7cfa3bc58cbd
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-04-01 00:12:11 -0300

    update queue intregation

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify
-from PIL import Image
-import io
-import cv2
-import hashlib
+from controllers.runner import Runner
 
 app = Flask(__name__)
+runner = Runner()
 
 
 @app.route('/', methods=['GET'])
@@ -15,21 +13,13 @@ def index():
 
 @app.route('/send', methods=['POST'])
 def send():
-    try:
-        image_bytes = request.data
-        if image_bytes == b'':
-            return jsonify(message='Envie uma imagem'), 418
-        if not request.content_type.startswith('image'):
-            return jsonify(message='Envie uma imagem valida'), 415
-        image_key = hashlib.md5(image_bytes).hexdigest()
-        image_path = image_key + '.png'
-        image = Image.open(io.BytesIO(image_bytes))
-        image = cv2.resize(image, (384, 384))
-        image.save(image_path)
-        return jsonify(file_name=image_path), 201
-    except Exception as exception:
-        print(str(exception))
-        return jsonify(message='algum erro ocorreu, tente novamente como binary data'), 500
+    image_bytes = request.data
+    if image_bytes == b'':
+        return jsonify(message='Envie uma imagem'), 418
+    if not request.content_type.startswith('image'):
+        return jsonify(message='Envie uma imagem valida'), 415
+    name = runner.send(image_bytes)
+    return jsonify(file_name=name), 201
 
 
 @app.route('/test', methods=['GET'])
@@ -38,4 +28,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=4000, threaded=True)
---
+++ app/controllers/runner.py
@@ -0,0 +1,18 @@
+import pika
+import hashlib
+
+
+class Runner(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='images')
+
+    def send(self, image_binary):
+        self.channel.basic_publish(
+            exchange='', routing_key='images', body=image_binary)
+        return hashlib.md5(image_binary).hexdigest() + ".png"
+
+    def close(self):
+        self.connection.close()
