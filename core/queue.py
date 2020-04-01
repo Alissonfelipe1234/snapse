3c4b3f54ff6e611f1df61adf9a5e5fe895e8677d
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-04-01 00:12:36 -0300

    implemented core of resize

---
+++ core/queue.py
@@ -0,0 +1,18 @@
+import pika
+import snapse
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost', port=5672))
+channel = connection.channel()
+
+channel.queue_declare(queue='images')
+
+
+def callback(ch, method, properties, body):
+    snapse.resize(body)
+
+
+channel.basic_consume(
+    queue='images', on_message_callback=callback, auto_ack=True)
+
+print('Queue is started. To exit press CTRL+C')
+channel.start_consuming()
---
+++ core/snapse.py
@@ -0,0 +1,11 @@
+import hashlib
+from PIL import Image
+import io
+
+
+def resize(image_bytes):
+    image_key = hashlib.md5(image_bytes).hexdigest()
+    image_path = 'images/' + image_key + '.png'
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.resize((384, 384), Image.ANTIALIAS)
+    image.save(image_path)
