ebab4aa8083a7426ac207fb89d74d85b06e7779f
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-04-01 00:10:14 -0300

    implement test for queue

---
+++ core/test.py
@@ -0,0 +1,17 @@
+import unittest
+import hashlib
+import snapse
+
+
+class ApiTestCase(unittest.TestCase):
+    def test_api_test(self):
+        with open('images/marie_curie.jpg', 'rb') as img:
+            binary = img.read()
+            snapse.resize(binary)
+            file_name = 'images/' + hashlib.md5(binary).hexdigest() + '.png'
+            with open(file_name, 'rb') as small_img:
+                self.assertIsNotNone(small_img)
+
+
+if __name__ == '__main__':
+    unittest.main()
