adaaaaa00e481b4c3582fa9a37507235699a558e
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-04-01 23:47:58 -0300

    path relative fix

--- core/test.py
+++ core/test.py
@@ -5,10 +5,10 @@ import snapse
 
 class ApiTestCase(unittest.TestCase):
     def test_api_test(self):
-        with open('images/marie_curie.jpg', 'rb') as img:
+        with open('./images/marie_curie.jpg', 'rb') as img:
             binary = img.read()
             snapse.resize(binary)
-            file_name = 'images/' + hashlib.md5(binary).hexdigest() + '.png'
+            file_name = './images/' + hashlib.md5(binary).hexdigest() + '.png'
             with open(file_name, 'rb') as small_img:
                 self.assertIsNotNone(small_img)
 
