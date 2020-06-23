5ce6159ceae6985308640840ae91eb8b53099065
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-06-22 21:57:17 -0400

    update tests

--- app/test.py
+++ app/test.py
@@ -8,7 +8,10 @@ class ApiTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='application/json')
         expected = b'{"valid_paths":{"/":"home",' + \
-            b'"/resize":"resize images, use header width and height","/test":"test request"}}\n'
+            b'"/filter/<kinds>":"filter images, ' + \
+            b'kinds(blur, contour, detail, emboss, edges),"' + \
+            b',"/resize":"resize images, use header width and height",' + \
+            b'"/test":"test request"}}\n'
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, expected)
 
@@ -19,37 +22,51 @@ class ApiTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, expected)
 
-    def test_api_send(self):
+    def test_api_filter(self):
         tester = app.test_client(self)
         content = 'image/jpg'
         binary = b'zeros_e_uns'
-        name_file = hashlib.md5(binary).hexdigest()
-        response = tester.post('/resize', content_type=content, data=binary, headers={'width': '200', 'height': '200'})
+        hashed = binary + bytes("blur", encoding='utf-8')
+        name_file = hashlib.md5(hashed).hexdigest()
+        response = tester.post('/filter/blur',
+                               content_type=content, data=binary)
         expected = '{"file_name":"' + name_file + '.png"}\n'
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data.decode('utf-8'), expected)
 
-    def test_api_send_without_height(self):
+    def test_api_resize(self):
         tester = app.test_client(self)
         content = 'image/jpg'
         binary = b'zeros_e_uns'
-        name_file = hashlib.md5(binary).hexdigest()
-        response = tester.post('/resize', content_type=content, data=binary, headers={'width': '200'})
+        name_file = hashlib.md5(binary + bytes(200) + bytes(200)).hexdigest()
+        response = tester.post('/resize',
+                               content_type=content, data=binary,
+                               headers={'width': '200', 'width': '200'})
+        expected = '{"file_name":"' + name_file + '.png"}\n'
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data.decode('utf-8'), expected)
+
+    def test_api_resize_without_height(self):
+        tester = app.test_client(self)
+        content = 'image/jpg'
+        binary = b'zeros_e_uns'
+        response = tester.post('/resize',
+                               content_type=content, data=binary,
+                               headers={'width': '200'})
         expected = '{"message":"use the height`s header"}\n'
         self.assertEqual(response.status_code, 418)
         self.assertEqual(response.data.decode('utf-8'), expected)
 
-    def test_api_send_without_width(self):
+    def test_api_resize_without_width(self):
         tester = app.test_client(self)
         content = 'image/jpg'
         binary = b'zeros_e_uns'
-        name_file = hashlib.md5(binary).hexdigest()
         response = tester.post('/resize', content_type=content, data=binary)
         expected = '{"message":"use the width`s header"}\n'
         self.assertEqual(response.status_code, 418)
         self.assertEqual(response.data.decode('utf-8'), expected)
 
-    def test_api_send_empty(self):
+    def test_api_resize_empty(self):
         tester = app.test_client(self)
         content = 'application/json'
         response = tester.post('/resize', content_type=content)
@@ -57,7 +74,7 @@ class ApiTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 418)
         self.assertEqual(response.data, expected)
 
-    def test_api_send_invalid(self):
+    def test_api_resize_invalid(self):
         tester = app.test_client(self)
         content = 'application/json'
         response = tester.post('/resize', content_type=content, data=b'imagem')
